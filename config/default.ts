969ceb94c4eeec23ba8413b8c6948e8f546b8f2c
Lucas Fernandes <lcsfernandesg@gmail.com>
2026-05-05 20:04:15 -0300

    update

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,10 @@
+
+    
+const dbUser = process.env.DB_USER 
+const  dbPassword = process.env.DB_PASSWORD 
+
+
 export default {
     port: 3000,
-    
+    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.kbb9ety.mongodb.net/?appName=Cluster0`
 }
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+//ENV variables
+require("dotenv").config();
+
 import express from "express"
 import config from "config"
 
@@ -6,11 +9,21 @@ const app = express()
 //JSON middleware
 app.use(express.json())
 
+//DB
+import db from "../config/db"
+
+
+//ROUTES
+import router from "./router"
+
+
+app.use("/api/", router)
 
 //app port
 const port = config.get<number>("port")
 
 
 app.listen(port, async () => {
+    await db()
     console.log(`Aplicacao funcionando na porta: ${port}`)
 })
